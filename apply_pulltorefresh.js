45b7a354115139944f9dbfce655e01d0e613343e
Andrei Cristian Petcu <andrei@ceata.org>
2018-05-30 19:14:54 +0300

    fixed bugzilla issue

--- apply_pulltorefresh.js
+++ apply_pulltorefresh.js
@@ -1,4 +1,18 @@
+
+const domainToElementSelector = [];
+domainToElementSelector['bugzilla.mozilla.org'] = '#bugzilla-body';
+
+var element = domainToElementSelector[document.location.host];
+
+if(!element){
+  element = 'body';
+}
+
+console.log(`using element ${element}`);
+
 var ptr = PullToRefresh.init({
-    mainElement: 'body',
-    onRefresh: function(){ window.location.reload(); }
-  });
+  mainElement: element,
+  onRefresh: function () {
+    window.location.reload();
+  }
+});
